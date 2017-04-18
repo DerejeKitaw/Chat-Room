b15544a4629e0ca65a3a09cb0b856f9faf36c1aa
Dereje Kitaw <dkitaw@solareworld.local>
2017-04-18 17:16:16 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+
+var app = express();
+
+app.set("views", "./views");
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+app.use(express.static('node_modules/bootstrap/dist'));
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res){
+    res.render("index.pug", {title: "Index"});
+});
+app.get('/admin/rooms', function(req, res){
+    res.render("rooms",{title:"Admin Rooms"} );
+});
+app.listen (3000,function(){
+console.log('Chat app listening on port 3000!');
+});
