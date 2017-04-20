de9a3ac3218c498dad9966a743557eb28b19ff4a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-19 21:56:57 -0400

    rooms printed in room view

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var rooms = require('./data/Rooms.json');
 
 app.set("views", "./views");
 app.set('view engine', 'pug');
@@ -14,7 +15,10 @@ app.get('/', function(req, res){
     res.render("index.pug", {title: "Index"});
 });
 app.get('/admin/rooms', function(req, res){
-    res.render("rooms",{title:"Admin Rooms"} );
+    res.render("rooms",{
+        title:"Admin Rooms",
+        rooms: rooms
+} );
 });
 app.listen (3000,function(){
 console.log('Chat app listening on port 3000!');
