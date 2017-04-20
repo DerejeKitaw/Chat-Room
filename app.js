78d653a09a43f2a9f43f23c6033b1da60ecdd0b4
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-19 23:21:11 -0400

    bodyParsor installed and included

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 var rooms = require('./data/Rooms.json');
+var bodyParser=require("body-parser");
 
 app.set("views", "./views");
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
