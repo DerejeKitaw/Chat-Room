b11de3fd29a7bea840788d4b1d243182368999ef
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-20 22:50:15 -0400

    /api/rooms route defined to send rooms json

--- api.js
+++ api.js
@@ -1,4 +1,9 @@
 var express =require("express");
+var rooms = require("./data/rooms/json");
 
 var router = express.Router();
 module.exports= router;
+//Define various route
+router.get("/rooms",function(req,res){
+    res.json(rooms);
+});
