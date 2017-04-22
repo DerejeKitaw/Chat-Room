9905fb08f5d8074e9323b342f66f5ba08bb8b4a3
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-21 20:46:18 -0400

    get messages of specific room and messages.json file added

--- api.js
+++ api.js
@@ -1,9 +1,29 @@
 var express =require("express");
 var rooms = require("./data/rooms.json");
+var messages = require("./data/messages.json");
+var _=require("lodash");
 
 var router = express.Router();
 module.exports= router;
-//Define various route
+//Define various route - get all rooms
 router.get("/rooms",function(req,res){
     res.json(rooms);
+});
+
+//get messages of specific room
+router.get("/rooms/:roomId/messages", function (req, res){
+    var roomId = req.params.roomId;
+    //filter messages with roomId
+    var roomMessages = messages
+        .filter(m => m.roomId === roomId);
+    
+    var room = _.find(rooms, r => r.id === roomId);
+    if (!room){
+        res.sendStatus(404);
+        return;
+    }
+    res.json({
+        room: room,
+        messages: roomMessages
+    })
 });
