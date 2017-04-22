ab8143866afe138984acec1ae2e7eda1028bfc45
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-21 23:09:48 -0400

    GET and getMessage function defied

--- public/home.js
+++ public/home.js
@@ -1,3 +1,34 @@
-$(function(){
-    
+$(function () {
+
+    var roomId;
+
+$.ajax({
+        type: "GET",
+        url: "/api/rooms",
+        success: (function (rooms) {
+        roomId = rooms[0].id;
+        getMessages();
+        $.each(rooms, function (key, room) {
+            var a = '<a href="#" data-room-id="' + room.id + '" class="room list-group-item">' + room.name + '</a>';
+            $("#rooms").append(a);
+        });
+
+    })
+    })
+    function getMessages() {
+        $.ajax({
+            type: "GET",
+            url: "/api/rooms/" + roomId + "/messages",
+            success: (function (data) {
+            $("#roomName").text("Messages for " + data.room.name);
+            var messages = "";
+            $.each(data.messages, function (key, message) {
+                messages += message.text + "\r";
+            });
+            $("#messages").val(messages);
+        })
+        })
+    }
+   
+
 });
