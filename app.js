4dafcf7ca3235d8a2cc61ab95fb2caafc3e05c37
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-23 17:45:47 -0400

    Code commented

--- app.js
+++ app.js
@@ -3,26 +3,37 @@ var app = express();
 
 var bodyParser=require("body-parser");
 
+//Settings
 app.set("views", "./views");
 app.set('view engine', 'pug');
 
+//Express midleware
 app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/bootstrap/dist'));
+
+//To get form data from the form
 app.use(bodyParser.urlencoded({extended: true}));
- 
+
+//To get json data from the form
+app.use(bodyParser.json());
+
 //TODO: Create a middleware to log all incomming request -- Done partialy
 app.use(function(req,res,next){
     console.log('Incoming request: ' + req.url);
     next();
 });
 
+//root page is "home"
 app.get('/', function (req, res) {
     res.render("home.pug", { title: "Home" });
 });
- var adminRouter = require("./admin");
+
+//admin router mounted at /admin
+var adminRouter = require("./admin");
  app.use("/admin",adminRouter);
-//create new api router
+
+//apiRouter router mounted at /api
 var apiRouter = require("./api");
 app.use("/api",apiRouter);
 
